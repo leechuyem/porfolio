aaa160e6bce61f53f8e3d0eb00d1aa5336f8e7c0
Lee Chu Yem <leechuyem@gmail.com>
2019-12-23 05:16:28 +1100

    change title dynamically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,41 @@
 import { Component, OnInit } from "@angular/core";
 import { Router, NavigationEnd } from "@angular/router";
-import { RouteUrl } from 'src/app/models/route-url.enum';
-import { Styles } from '../app/misc/const';
+import { Title } from "@angular/platform-browser";
+
+import { RouteUrl } from "src/app/models/route-url.enum";
+import { Styles } from "../app/misc/const";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ['./app.component.scss']
+  styleUrls: ["./app.component.scss"]
 })
-
 export class AppComponent implements OnInit {
+  currentPage = "";
+  titles = ["Lee Chu Yem 🚀", "Lee Chu Yem | About", "Lee Chu Yem | Projects"];
+
   style = {
     primaryColour: "",
     secondaryColour: "",
     text: ""
   };
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private titleService: Title) {}
 
   ngOnInit() {
     this.router.events.subscribe((event: any) => {
       if (event instanceof NavigationEnd) {
         let url = this.router.url;
-        this.style = Styles[RouteUrl[url]];
+
+        if (url != this.currentPage) {
+          this.style = Styles[RouteUrl[url]];
+          this.setTitle(this.titles[RouteUrl[url]]);
+        }
       }
     });
+  }
+
+  public setTitle(newTitle: string) {
+    this.titleService.setTitle(newTitle);
   }
 }
