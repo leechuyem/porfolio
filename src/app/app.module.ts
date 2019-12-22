9db34e396c1764af768787783164d676172dd495
Lee Chu Yem <leechuyem@gmail.com>
2019-12-22 14:19:55 +1100

    add button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { ProjectsComponent } from './pages/projects/projects.component';
     HomeComponent,
     AboutComponent,
     ProjectsComponent,
+    ButtonComponent,
   ],
   imports: [
     BrowserModule,
