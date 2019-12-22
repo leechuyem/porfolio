2dad83f26ffdfa5a0db04d2c75e0ee463d4dce4c
Lee Chu Yem <leechuyem@gmail.com>
2019-12-23 00:55:26 +1100

    add page colours const

---
+++ src/app/misc/const.ts
@@ -0,0 +1,20 @@
+export const Styles = [
+  {
+    route: "/home",
+    text: "LEE CHU YEM",
+    primaryColour: "#143D59",
+    secondaryColour: "#F4B41A"
+  },
+  {
+    route: "/about",
+    text: "WHO AM I?",
+    primaryColour: "#00539C",
+    secondaryColour: "#FF9552"
+  },
+  {
+    route: "/projects",
+    text: "PROJECTS",
+    primaryColour: "#283350",
+    secondaryColour: "#f93800"
+  }
+];
