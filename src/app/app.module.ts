91f70ec6deef182c4c4de9d3bfd688e70f825e09
Lee Chu Yem <leechuyem@gmail.com>
2019-12-22 18:00:13 +1100

    add social-medial-links component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { ButtonComponent } from './components/button/button.component';
+import { SocialMediaLinksComponent } from './components/social-media-links/social-media-links.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { ButtonComponent } from './components/button/button.component';
     AboutComponent,
     ProjectsComponent,
     ButtonComponent,
+    SocialMediaLinksComponent,
   ],
   imports: [
     BrowserModule,
