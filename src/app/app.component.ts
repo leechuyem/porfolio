2d5c2a30f6307f6f3a5c0b3649c527bd80bf3111
Lee Chu Yem <leechuyem@gmail.com>
2019-12-23 00:56:13 +1100

    move the router event from heading to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Router, NavigationEnd } from "@angular/router";
+import { RouteUrl } from 'src/app/models/route-url.enum';
+import { Styles } from '../app/misc/const';
 
 @Component({
   selector: "app-root",
@@ -6,6 +9,21 @@ import { Component } from "@angular/core";
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+  style = {
+    primaryColour: "",
+    secondaryColour: "",
+    text: ""
+  };
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event: any) => {
+      if (event instanceof NavigationEnd) {
+        let url = this.router.url;
+        this.style = Styles[RouteUrl[url]];
+      }
+    });
+  }
 }
