d6d9395e5cb49d6994bab43af8593390c4c10081
Lee Chu Yem <leechuyem@gmail.com>
2019-12-23 00:34:33 +1100

    add project page

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import Data from "../../../assets/projects.json";
+import { Project } from "../../models/projects";
 
 @Component({
-  selector: 'app-projects',
-  templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  selector: "app-projects",
+  templateUrl: "./projects.component.html",
+  styleUrls: ["./projects.component.scss"]
 })
 export class ProjectsComponent implements OnInit {
+  projects: Project[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.projects = Data as Project[];
   }
-
 }
