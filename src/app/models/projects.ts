500402c9bc3692ff6c983c123c384fd912e5830c
Lee Chu Yem <leechuyem@gmail.com>
2019-12-23 00:34:15 +1100

    - add project json - add project icons - add project interface

---
+++ src/app/models/projects.ts
@@ -0,0 +1,11 @@
+export interface Project {
+  name: string;
+  desc: string;
+  languages: string[];
+  link: string;
+  img: string;
+  primaryColour: string;
+  secondaryColour: string;
+  l1TxtColour: string;
+  l2TxtColour: string;
+}
